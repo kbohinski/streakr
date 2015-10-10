faadec4397ba26dd19fd45eeff23c4a7a4c35f01
Kevin Bohinski <kbohinski@gmail.com>
2015-10-10 01:46:49 -0400

    Windows works now

--- installer.ps1
+++ installer.ps1
@@ -1,7 +1,60 @@
-$action = New-ScheduledTaskAction -Execute 'C:\streakr\streakr.ps1' `
+$action = New-ScheduledTaskAction -Execute  'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -NonInteractive -File "C:\streakr\streakr.ps1"'
 
-  -Argument '-NoProfile -WindowStyle Hidden'
+$trigger =  New-ScheduledTaskTrigger -Daily -At 1pm
 
-$trigger =  New-ScheduledTaskTrigger -Daily -At 9am
+$user = Read-Host ("Please enter your windows username in domain\user format")
+$pass = Read-Host ("Please enter the password for " + $user) -AsSecureString
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass)
+$pass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+$xml = '<?xml version="1.0" encoding="UTF-16"?>
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Date>2015-10-10T00:00:00.0000000</Date>
+    <Author>Administrator</Author>
+  </RegistrationInfo>
+  <Triggers>
+    <CalendarTrigger>
+      <StartBoundary>2015-10-10T13:00:00</StartBoundary>
+      <Enabled>true</Enabled>
+      <ScheduleByDay>
+        <DaysInterval>1</DaysInterval>
+      </ScheduleByDay>
+    </CalendarTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>$user</UserId>
+      <LogonType>Password</LogonType>
+      <RunLevel>LeastPrivilege</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
+    <AllowHardTerminate>true</AllowHardTerminate>
+    <StartWhenAvailable>false</StartWhenAvailable>
+    <RunOnlyIfNetworkAvailable>true</RunOnlyIfNetworkAvailable>
+    <IdleSettings>
+      <StopOnIdleEnd>true</StopOnIdleEnd>
+      <RestartOnIdle>false</RestartOnIdle>
+    </IdleSettings>
+    <AllowStartOnDemand>true</AllowStartOnDemand>
+    <Enabled>true</Enabled>
+    <Hidden>false</Hidden>
+    <RunOnlyIfIdle>false</RunOnlyIfIdle>
+    <WakeToRun>true</WakeToRun>
+    <ExecutionTimeLimit>P3D</ExecutionTimeLimit>
+    <Priority>7</Priority>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe</Command>
+      <Arguments>-NoLogo -NonInteractive -File "C:\streakr\streakr.ps1"</Arguments>
+    </Exec>
+  </Actions>
+</Task>'
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "streakr" -Description "http://github.com/kbohinski/streakr"
+Write-Output $xml
+
+Register-ScheduledTask -TaskName "streakr" -TaskPath "\" -Xml $xml -User $user -Password $pass
